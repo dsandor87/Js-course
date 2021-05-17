21fcc24e1635646481ab05676206b8fd3c0a4ca5
Sándor Dési <d.alex.galaxy@gmail.com>
2021-05-17 11:25:59 +0100

    Day 1

---
+++ Primitives/main.js
@@ -0,0 +1,35 @@
+
+
+let input = window.prompt("What is the day today?: ")
+let day = input.trim().toLowerCase()
+
+console.log(day)
+
+switch(day)
+{
+    case 'monday':
+    alert('Im starting the week reading my emails')
+    break;
+    case 'tuesday':
+    alert('Im do the normal 9-5 working, go out for a walk ')
+    break;
+    case 'wednesday':
+    alert('Im star working and after work go to shopping')
+    break;
+    case 'thursday':
+    alert('Im starting the week with morning run')
+    break;
+    case 'friday':
+    alert('Im working on my projects')
+    break;
+    case 'saturday':
+    alert('Spend time with friends')
+    break;
+    case 'sunday':
+    alert('Have a nioce Sunday-Roast')
+    break;
+ default :
+ alert('Something went wrong')
+
+
+}
