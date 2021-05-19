e2f88de7bee0a2c158e5dca5c9aed3892722db99
Sándor Dési <d.alex.galaxy@gmail.com>
2021-05-19 15:55:51 +0100

    Day 3

--- Calendar/main.js
+++ Calendar/main.js
@@ -10,8 +10,7 @@ var options = {
       ticketType: 'VIP'
     },
     onClick: function(event, timetable, clickEvent) {
-
-        var name = window.prompt("Add Your Activity name: ");
+        var name = window.prompt("Activity name: ");
         var start = window.prompt("Starts at: ")
         var end = window.prompt("Finished at: ")
 
@@ -21,16 +20,39 @@ var options = {
 
     } // custom click handler, which is passed the event object and full timetable as context  
   };
-timetable.addLocations(['Monday', 'Tuesday', 'Wednesday', 'Thursday','Friday']);
-timetable.addEvent('Project management', 'Tuesday', new Date(2015,7,17,10,45), new Date(2015,7,17,12,30),);
-timetable.addEvent('Project management', 'Monday', new Date(2015,7,17,9), new Date(2015,7,17,10,45),options);
 
+  const points = [
+      ]
+timetable.addLocations(['Monday', 'Tuesday', 'Wednesday', 'Thursday','Friday']);
+// timetable.addEvent('Project management', 'Tuesday', new Date(2015,7,17,10,45), new Date(2015,7,17,12,30),);
+timetable.addEvent('Project management', 'Monday', new Date(2015,7,17,9), new Date(2015,7,17,10,45));
+timetable.addEvent('Project management', 'Monday', new Date(2015,7,17,9), new Date(2015,7,17,10,45));
+
+points.forEach((slot, index, array) => {
+    timetable.addEvent(slot.name, slot.day, new Date(2015,7,17,slot.start), new Date(2015,7,17,slot.end,45));
+
+});
 var renderer = new Timetable.Renderer(timetable);
 renderer.draw('.timetable'); 
 
-function myFunction (){
-    console.log('clicked')
-    var name = window.prompt("Add Your Activity name: ");
-        var start = window.prompt("Starts at: ")
-        var end = window.prompt("Finished at: ")
+function getData (){
+slot = {}
+
+    var day = window.prompt("Which day would you like to book?");
+    var name = window.prompt("Add Your Activity name:");
+    var start = window.prompt("Starts at:")
+    var end = window.prompt("Finished at:")
+
+        slot.day = day
+        slot.name = name;
+        slot.start = start;
+        slot.end = end;
+        
+points.push(slot)
+
+console.log(slot)
+
 }
+
+console.log(points)
+
